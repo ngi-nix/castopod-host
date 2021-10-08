4845262e4d6b702dd063ca191b0dffc35b2939c6
Charlie Hanley <charles.scott.hanley+git@gmail.com>
2021-10-08 02:30:56 -0400

    rm package source; refer to it remotely

--- app/Resources/js/podcast.ts
+++
@@ -1,9 +0,0 @@
-import Dropdown from "./modules/Dropdown";
-import Time from "./modules/Time";
-import Toggler from "./modules/Toggler";
-import Tooltip from "./modules/Tooltip";
-
-Dropdown();
-Time();
-Toggler();
-Tooltip();
